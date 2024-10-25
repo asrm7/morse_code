5f6c76467246758f61372a5a1ab9bcf4c5fd60c8
Alex Sandro Rodrigues Medeiros <rodrigues.medeiros@hotmail.com>
2024-10-25 14:36:13 +0100

    app

---
+++ app.py
@@ -0,0 +1,30 @@
+# Morse code
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+    'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.', '0': '-----', ' ': ' ', '.': '.-.-.-',
+    ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+    ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.',
+    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+}
+
+# Function to encode the message
+def encode_to_morse(message):
+    
+    #here where you going to code
+
+
+# Function to decode the message
+def decode_from_morse(message):
+    #here where you going to code
+
+
+
+message = input("Enter the message >>")
+
+encode_to_morse(message)
+print(f"Encoded message: {message}")
+print(f"Decoded message: {message}")
